3123f36dd7f7d27a098ed36a2f941b2bfe967674
leesw <leesw@DESKTOP-UT2THB4>
2020-03-25 10:44:25 +0900

    [2020-03-25] - 소스정리

--- chpater5.eurekaserver/src/main/java/com/brownfield/pss/eurekaserver/EurekaserverApplication.java
+++ chpater5.eurekaserver/src/main/java/com/brownfield/pss/eurekaserver/EurekaserverApplication.java
@@ -1,4 +1,4 @@
-package com.borwnfield.pss.eurekaserver;
+package com.brownfield.pss.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
